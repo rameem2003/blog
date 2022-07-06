60c83e832873212ca34c433dba7bf3d79c8246c8
rameem2003 <rameem2019@gmail.com>
2022-07-06 21:43:26 +0600

    responsive

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const tog = document.getElementById("tog");
+const menu = document.getElementById("menu");
+
+
+tog.addEventListener("click", () => {
+    menu.classList.toggle("active_toggle");
+})
